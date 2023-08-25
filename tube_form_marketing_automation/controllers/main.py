b124d1a7fd7c2e82567c0edac84f76bc1d41c67b
bhargavbharadiya <bhargav.bharadiya@silentinfotech.com>
2023-08-25 18:45:53 +0530

    [31895 31898 31900] Workflow 1: Form completion notification , Send this email to the contact Us form , After 1 week, Send second email to the contact completed

--- tube_form_marketing_automation/controllers/main.py
+++ tube_form_marketing_automation/controllers/main.py
@@ -4,14 +4,14 @@ from odoo import http
 from odoo.http import request
 
 class propertModel(http.Controller):
-    @http.route(['/unsubscribe-email'], auth='public',type='http', website=True)
+    @http.route(['/unsubscribe-email'], auth='public', type='http', website=True)
     def unsubscribe_email_model(self, **post):
-
         user_email = post.get('email')
-        customer = request.env['res.partner'].search([('email','=',user_email)],limit=1)
+        customer = request.env['res.partner'].search([('email', '=', user_email)], limit=1)
         base_url = customer.get_base_url()
 
         if customer.is_blacklist != True:
             customer.sudo().write({'is_blacklist': True,
                                    'unsubscription_date': datetime.datetime.now()})
+        request.env.cr.commit()
         return request.redirect(base_url)
--- tube_form_marketing_automation/models/res_partner.py
+++ tube_form_marketing_automation/models/res_partner.py
@@ -12,7 +12,7 @@ class ResPartner(models.Model):
     unsubscription_date = fields.Datetime(string="Unsubscription Date")
 
     def completed_contact_us_form_cron(self):
-        contact_list = self.env["res.partner"].search([('contact_us_email_notify_date','=',[datetime.date.today()])])
+        contact_list = self.env["res.partner"].search([('contact_us_email_notify_date','=',datetime.date.today())])
         contact_us_after_week_notify_email_template = self.env.ref('tube_form_marketing_automation.completed_contact_us_form_after_week_template')
 
         for customer in contact_list:
