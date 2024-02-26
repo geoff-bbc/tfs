16c443c1372da44e86c5e8c3cc68c6020708affa
Bluebird Cloud Consulting <geoff@blue-bird.cloud>
2024-02-26 14:49:30 -0800

    Update main.py

--- bbc_extensions/controllers/main.py
+++ bbc_extensions/controllers/main.py
@@ -12,3 +12,18 @@ class TestModelWizard(models.TransientModel):
         records = self.env['test.model'].browse(self.env.context.get('active_ids'))
         for rec in records: 
             rec.write({'state' : 'done'})
+            
+class NoQuotationSaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_quotation_send(self):
+    self.ensure_one()
+
+    if self.state != "sale":
+    return False
+
+    # Use this if you want to use the access token in your email-template:
+    # self._portal_ensure_token()
+
+    return super(NoQuotationSaleOrder, self).action_quotation_send()
