442f7b2de2583534c7b19e62417a893d1ac61f3b
Bluebird Cloud Consulting <geoff@blue-bird.cloud>
2024-02-25 19:35:35 -0800

    updates

--- bbc_extensions/controllers/main.py
+++ bbc_extensions/controllers/main.py
@@ -3,11 +3,11 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class TestModelWizard(models.TransientModel):
-    _name =  ‘test.model.wizard’
-    _description = ‘Test Model Wizard’
-    test_field = fields.Char(string = ‘Test Field’)
+    _name =  'test.model.wizard'
+    _description = 'Test Model Wizard'
+    test_field = fields.Char(string = 'Test Field')
     
     def action_done(self):
         records = self.env['test.model'].browse(self.env.context.get('active_ids'))
         for rec in records: 
-            rec.write({'state' : ’done’})
+            rec.write({'state' : 'done'})
