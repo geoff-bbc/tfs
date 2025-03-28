5fa29d8667e22ba4dccab3138dc9e8de90cc4f98
Bluebird Cloud Consulting <geoff@blue-bird.cloud>
2025-03-28 15:31:51 -0700

    prep for v18

--- purchase_vendorbill_advance/__init__.py
+++ purchase_vendorbill_advance/__init__.py
@@ -4,20 +4,16 @@
 from . import models
 from . import wizard
 
-from odoo import api, SUPERUSER_ID
 
-
-def _post_init_hook(cr, registry):
+def _post_init_hook(env):
     """Deactivate the server action 'Create Vendor Bills' once this app is installed"""
-    env = api.Environment(cr, SUPERUSER_ID, {})
     vendor_bill_server_action = env.ref("purchase.action_purchase_batch_bills")
     if vendor_bill_server_action:
         vendor_bill_server_action.write({"binding_model_id": ""})
 
 
-def _uninstall_hook(cr, registry):
+def _uninstall_hook(env):
     """Activate the server action 'Create Vendor Bills' once this app is uninstalled"""
-    env = api.Environment(cr, SUPERUSER_ID, {})
     model = env["ir.model"].search([("model", "=", "purchase.order")])
     vendor_bill_server_action = env.ref("purchase.action_purchase_batch_bills")
 
