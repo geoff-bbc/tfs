9894d2065e9b01d2f2f43cade3f8f8e447777cfe
Bluebird Cloud Consulting <geoff@blue-bird.cloud>
2025-03-28 17:04:40 -0700

    remove marketing_automation

--- tube_form_marketing_automation/controllers/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import  main
-from . import website_mail_main
--- tube_form_marketing_automation/models/res_config_settings.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-class ResConfigSettingsInherit(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    follow_up_2_notify_day = fields.Integer(string='2nd Followup Delay (In Days)',config_parameter = "follow_up_2_notify_day")
-    follow_up_3_notify_day = fields.Integer(string='3rd Followup Delay (In Days)',config_parameter = "follow_up_3_notify_day")
-
